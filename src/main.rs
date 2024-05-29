f2af68e2b7d4c50cb8b9f9d2688b36fbf7e07192
GearBoxFox <favoringbasil55@gmail.com>
2024-05-28 22:37:49 -0400

    Created libinput linux and windows listeners

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{Mutex, Arc};
+use std::time;
+use std::io::Error;
+
+mod programmable_keys;
+use crate::programmable_keys::programmable_keys::ProgrammableKeys;
+
+const QUEUE_CHECKING_DELAY: time::Duration = time::Duration::from_millis(20);
+
+#[cfg(target_os = "linux")]
+mod linux_listner;
+
+#[cfg(target_os = "windows")]
+mod window_listener;
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() -> Result<(), Error> {
+    let programmable_keys_vec: Vec<ProgrammableKeys> = Vec::new();
+    let programmable_keys_arc = Arc::new(Mutex::new(programmable_keys_vec));
+
+    let queue = programmable_keys_arc.clone();
+    tokio::spawn(async move{
+        loop {
+            std::thread::sleep(QUEUE_CHECKING_DELAY);
+
+            let retrieved_key = match queue.lock() {
+                Ok(mut borrowed_queue) => {
+                    borrowed_queue.pop()
+                },
+                Err(e) => {
+                    eprintln!("Error locking queue: {:?}", e);
+                    None
+                }
+            };
+
+            match retrieved_key {
+                Some(key) => {
+                    ProgrammableKeys::process_keys(key).await;
+                },
+                None => {}
+            }
+        }
+    });
+
+    #[cfg(target_os = "linux")]
+    linux_listner::linux_start(&programmable_keys_arc);
+
+    #[cfg(target_os = "windows")]
+    window_listener::windows_start(&programmable_keys_arc);
+
+    Ok(())
 }
