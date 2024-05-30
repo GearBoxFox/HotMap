c1cad956e8de0d035fd40aac8050f91a81e9763f
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-05-30 15:18:37 -0400

    added JS to main UI and worked on base elements

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,12 @@
+let keybindDiv
+let bsCollapse
 
+document.addEventListener("DOMContentLoaded", () => {
+    keybindDiv = document.getElementById("collapseWidthExample");
+    bsCollapse = new bootstrap.Collapse(keybindDiv);
+})
+
+function collapseClick() {
+    console.log("I was clicked!")
+    bsCollapse.toggle();
+}
